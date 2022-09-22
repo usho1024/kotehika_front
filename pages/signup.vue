e352c7f43a9598087ca0458034e03f60a01f7241
usho1024 <usho1024@gmail.com>
2022-09-22 23:27:15 +0900

    Add user form components

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    signup.vue
-  </div>
+  <user-form-card>
+    <template
+      #user-form-card-content
+    >
+      <v-form
+        v-model="isValid"
+      >
+        <user-form-name />
+        <user-form-email />
+        <user-form-password />
+        <!-- disabled=true => ボタンクリックを無効にする -->
+        <v-btn
+          :disabled="!isValid"
+          block
+          color="appblue"
+          class="white--text"
+        >
+          登録する
+        </v-btn>
+      </v-form>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
 export default {
-  layout: 'before-login'
+  layout: 'before-login',
+  data () {
+    return {
+      isValid: false
+    }
+  }
 }
 </script>
