2d8c2e63069234ef8e0fb3699fb914443654cda0
usho1024 <usho1024@gmail.com>
2022-09-26 13:34:15 +0900

    Add projects.vue layouts

--- plugins/my-inject.js
+++ plugins/my-inject.js
@@ -9,6 +9,19 @@ class MyInject {
     const title = this.app.i18n.t(jsonPath)
     return title
   }
+
+  // 日付のフォーマット変換
+  dateFormat (dateStr) {
+    const dateTimeFormat = new Intl.DateTimeFormat(
+      'ja', { dateStyle: 'medium', timeStyle: 'short' }
+    )
+    return dateTimeFormat.format(new Date(dateStr))
+  }
+
+  // プロジェクトリンク
+  projectLinkTo (id, name = 'project-id-dashboard') {
+    return { name, params: { id } }
+  }
 }
 
 export default ({ app }, inject) => {
